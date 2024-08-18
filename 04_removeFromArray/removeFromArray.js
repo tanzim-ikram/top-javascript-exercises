47810b6db81e8db5b7058f3bfe9dacbb02060800
Tanzim Ikram Sheikh <tanzim.ikram@gmail.com>
2024-08-18 17:17:30 +0600

    My Solution of 04_removeFromArray
    I was able to pass only 2 test cases

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, item) {
+    const newArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] !== item) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
