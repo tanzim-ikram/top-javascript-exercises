cd55647a8ea6c353799b882ab8ef326b14077b7e
Tanzim Ikram Sheikh <tanzim.ikram@gmail.com>
2024-08-18 18:31:38 +0600

    Solve 07_tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let tempCelsius = (temp - 32) * 5 / 9;
+  return parseFloat(tempCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let tempFahrenheit = (temp * 9 / 5) + 32;
+  return parseFloat(tempFahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
