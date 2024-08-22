dae2f32b7574470bc70ab1b9de0f33c71d02a03e
Tanzim Ikram Sheikh <tanzim.ikram@gmail.com>
2024-08-23 00:04:36 +0600

    Solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (countArg) {
+    let count
+    
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArg)
+    } else {
+        count = countArg
+    }
 
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+    const fib = [0, 1];
+    for (let i = 2; i <= count; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[count];
 };
 
 // Do not edit below this line
